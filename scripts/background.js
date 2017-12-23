3730554c8aecdaa8c5faed6772bad05506fd0502
Alejandro Munoz <amunoz797@gmail.com>
2017-12-23 00:39:26 -0500

    Updated accessory rendering to initialize on page load

--- scripts/background.js
+++ scripts/background.js
@@ -1,18 +1,23 @@
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-      if( request.message === "GET_KITTY" ) {
-          //Get kitty info from contract
-          console.log(request.kittyId);
-          let rawKittyInfo = KryptoKitties.getKitty(request.kittyId, function(error, result) {
-              if(error) {
-                  console.log(error);
-              }
-              else {
-                let kittyInfo = new KittyInfo(result[0], result[1], result[2].toJSON(), result[3].toJSON(), result[4].toJSON(), result[5].toJSON(), result[6].toJSON(), result[7].toJSON(), result[8].toJSON(), result[9].toJSON());
-                sendResponse({result: kittyInfo});
-            }
-          });
-      }
-      return true;
+        if( request.message === "GET_KITTY" ) {
+            //Get kitty info from contract
+            let rawKittyInfo = KryptoKitties.getKitty(request.kittyId, function(error, result) {
+                if(error) {
+                    console.log(error);
+                }
+                else {
+                    let kittyInfo = new KittyInfo(result[0], result[1], result[2].toJSON(), result[3].toJSON(), result[4].toJSON(), result[5].toJSON(), result[6].toJSON(), result[7].toJSON(), result[8].toJSON(), result[9].toJSON());
+                    sendResponse({result: kittyInfo});
+                }
+            });
+        }
+        return true;
+    }
+);
+
+chrome.tabs.onUpdated.addListener(
+    function(tabId, changeInfo, tab) {
+        chrome.tabs.sendMessage(tabId, {"message": "POLL_KITTIES"});
     }
 );
--- scripts/content.js
+++ scripts/content.js
@@ -21,42 +21,77 @@ let kittyAccessories = [
     bandaid
 ];
 
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-        if( request.message === "GET_KITTY_INFO" ) {
-            //Select all kitty items and extract kitty id
-            document.querySelectorAll(".KittiesGrid-item").forEach((div, index, array) => {
-                let kittyId = /\/\d*$/g.exec(div.childNodes[0].href)[0].substr(1);
+poll(renderAccessories, 10000, 100);
 
-                //Send kitty ids to background script
-                chrome.runtime.sendMessage({"message": "GET_KITTY", "kittyId": kittyId}, (response) => {
-                    console.log(response);
-                    let accessory = kittyAccessories[BigNumber(response.result.birthTime.slice(-1)).modulo(kittyAccessories.length).toNumber()].cloneNode();
-                    accessory.className = "KittyCard-image";
-                    accessory.style.position = "absolute";
-                    
-                    div.childNodes[0].childNodes[0].childNodes[0].appendChild(accessory);
-                });
-            });
-
-            //KittyBanner-container
-            document.querySelectorAll(".KittyBanner-container").forEach((div, index, array) => {
-                let kittyId = /\/\d*$/g.exec(div.childNodes[0].href)[0].substr(1);
-
-                //Send kitty ids to background script
-                chrome.runtime.sendMessage({"message": "GET_KITTY", "kittyId": kittyId}, (response) => {
-                    console.log(response);
-                    let accessoryContainer = document.createElement("a");
-                    accessoryContainer.style.position = "absolute";
-
-                    let accessory = kittyAccessories[BigNumber(response.result.birthTime.slice(-1)).modulo(kittyAccessories.length).toNumber()].cloneNode();
-                    accessory.className = "KittyBanner-image";
-                    accessoryContainer.appendChild(accessory);
-
-                    div.appendChild(accessoryContainer);
-                });
-            });
-        }
-        return true;
+chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
+    if( request.message === "GET_KITTY_INFO" ) {
+        // Select all kitty items and extract kitty id
+        renderAccessories();
     }
-);
+    else if( request.message === "POLL_KITTIES") {
+        poll(renderAccessories, 10000, 100);
+    }
+});
+
+function renderAccessories() {
+    if(document.querySelector(".KittiesGrid-item") == undefined && document.querySelector(".KittyBanner-container") == undefined) {
+        //Didn't find any kitties on the page
+        return false;
+    }
+
+    document.querySelectorAll(".KittiesGrid-item").forEach((div, index, array) => {
+        let kittyId = /\/\d*$/g.exec(div.childNodes[0].href)[0].substr(1);
+
+        //Send kitty ids to background script
+        chrome.runtime.sendMessage({"message": "GET_KITTY", "kittyId": kittyId}, (response) => {
+            let accessory = kittyAccessories[BigNumber(response.result.birthTime.slice(-1)).modulo(kittyAccessories.length).toNumber()].cloneNode();
+            accessory.className = "KittyCard-image";
+            accessory.style.position = "absolute";
+
+            div.childNodes[0].childNodes[0].childNodes[0].appendChild(accessory);
+        });
+    });
+
+    //KittyBanner-container
+    document.querySelectorAll(".KittyBanner-container").forEach((div, index, array) => {
+        let kittyId = /\/\d*$/g.exec(div.childNodes[0].href)[0].substr(1);
+
+        //Send kitty ids to background script
+        chrome.runtime.sendMessage({"message": "GET_KITTY", "kittyId": kittyId}, (response) => {
+            let accessoryContainer = document.createElement("a");
+            accessoryContainer.style.position = "absolute";
+
+            let accessory = kittyAccessories[BigNumber(response.result.birthTime.slice(-1)).modulo(kittyAccessories.length).toNumber()].cloneNode();
+            accessory.className = "KittyBanner-image";
+            accessoryContainer.appendChild(accessory);
+
+            div.appendChild(accessoryContainer);
+        });
+    });
+
+    return true;
+}
+
+// The polling function
+function poll(fn, timeout, interval) {
+    var endTime = Number(new Date()) + (timeout || 2000);
+    interval = interval || 100;
+
+    var checkCondition = function(resolve, reject) {
+        // If the condition is met, we're done! 
+        var result = fn();
+        if(result) {
+            resolve(result);
+        }
+        // If the condition isn't met but the timeout hasn't elapsed, go again
+        else if (Number(new Date()) < endTime) {
+            setTimeout(checkCondition, interval, resolve, reject);
+        }
+        // Didn't match and too much time, reject!
+        else {
+            reject(new Error('timed out for ' + fn + ': ' + arguments));
+        }
+    };
+
+    return new Promise(checkCondition);
+}
